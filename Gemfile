3544bd0c54158a64bdf8af7be17bbb523c65d364
shannonshsu <shannonshsu@gmail.com>
2015-05-31 01:37:27 +0000

    ch5

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'bootstrap-sass', '3.2.0.0'
 gem 'rails',        '4.2.0'
+gem 'bootstrap-sass', '3.2.0.0'
 gem 'sass-rails',   '5.0.2'
 gem 'uglifier',     '2.5.3'
 gem 'coffee-rails', '4.1.0'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,26 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title"
   end
-  
+
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title"
+  end
+  
   
 end
